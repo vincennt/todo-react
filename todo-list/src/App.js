9045809083a7ecfdeb9d6ca6655e4cfd4aa59496
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-02 11:53:57 +0100

    init files

---
+++ todo-list/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'components/form.js'
+
+class App extends React.Component{
+  
+  constructor() {
+    super()
+    this.state = {
+        placeHolder : "hello"
+    }
+  
+  }
+  render(){
+    return(<>
+    <Form/>
+    </>)
+  }
+}
+
+export default App
