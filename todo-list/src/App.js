ec6ba0bb999cc2a94e0bc63ac2a00f95d163b261
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-02 17:30:36 +0100

    add task a voir

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,20 +1,28 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from './components/form.js'
+import './App.css'
+
 
 class App extends React.Component{
   
   constructor() {
     super()
     this.state = {
-        placeHolder : "hello"
+      tasks: [],
+      
     }
-  
-  }
+    this.addTask = this.addTask.bind(this)
+    }
+    addTask(str){ 
+      this.setState(tasks = str)
+      console.log(str);
+      }
+    
   render(){
-    return(<>
-    <Form/>
-    </>)
+    return(
+    <Form addtask={this.addTask(this.props.task)} />
+    )
   }
 }
 
--- todo-list/src/components/form.js
+++ todo-list/src/components/form.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
 class Form extends React.Component{
-    render(){
-        return(<h1>hea</h1>)
+        constructor(){
+            super()
+            this.state = {
+                task: "",
+                
+            }
+            this.handleTaskDescriptionChange = this.handleTaskDescriptionChange.bind(this)
+            this.handleSubmit = this.handleSubmit.bind(this)
+        }
+       
+        handleTaskDescriptionChange(e){
+
+            this.setState({ task: e.target.value})
+        }
+        handleSubmit(e){
+            e.preventDefault()
+            this.setState({task : ""})
+            
+
+        }
+
+            render(){
+            return(<div> <form onSubmit={this.handleSubmit} className=" mt-5 d-flex justify-content-center gap-3">
+                <input value={this.state.task} onChange={this.handleTaskDescriptionChange}   type="text" className=" border border-dark rounded-pill form-control-plaintext h-25 w-25"
+                aria-label="Sizing example input" placeholder="Task"/>
+            <button type="submit" className=" h-50 btn btn-primary mb-3">Add list</button>
+            </form></div>)
+            }
+        
     }
-}
+
+    
 
 export default Form
